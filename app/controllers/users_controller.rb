dd1fa4abdba7408d68692b4bafb5d1b13ef0e9db
Igor.Koloski <koloski_igor@hotmail.com>
2021-06-24 09:45:25 +0200

    Add default photo and cover for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to :root, notice: 'User was successfully created.' }
+        format.html { redirect_to login_path, notice: 'User was successfully created.' }
         format.json { render :index, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   def current(id)
     return true if id == current_user.id
   end
+
+  def default_photo(photo)
+    return photo unless photo.file.nil?
+    'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'
+  end
+
+  def default_cover(cover)
+    return cover unless cover.file.nil?
+    'https://jordan-medical-services.com/wp-content/plugins/uix-page-builder/uixpb_templates/images/UixPageBuilderTmpl/default-cover-6.jpg'
+  end
 end
